948cddb9bdc14391aadf48c68fb1da57c8780d00
yasas <yasas@DESKTOP-A3HT73H>
2023-05-12 15:59:06 +0530

    Add project files.

---
+++ ThirasaraTest/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+public partial class TestClass : Form
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\thirasaradb.mdf;Integrated Security=True");
+
+    public TestClass()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        // TODO: This line of code loads data into the 'yourDatabaseNameDataSet.YourTableName' table. You can move, or remove it, as needed.
+        this.yourTableNameTableAdapter.Fill(this.yourDatabaseNameDataSet.test);
+    }
+}
